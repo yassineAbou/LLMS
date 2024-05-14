a2db61cfb0bcab90343515a8d60b65ae4b84bc71
yassineabou <aboyassine555@gmail.com>
2024-05-14 22:47:24 +0100

    Add app icons for Android, iOS, and desktop

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -81,6 +81,16 @@ compose.desktop {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "org.yassineabou.llms"
             packageVersion = "1.0.0"
+
+            macOS {
+                iconFile.set(project.file("icon.icns"))
+            }
+            windows {
+                iconFile.set(project.file("icon.ico"))
+            }
+            linux {
+                iconFile.set(project.file("icon.png"))
+            }
         }
     }
 }
--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,10 +1,13 @@
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
+    val icon = painterResource("drawable/icon.png")
     Window(
         onCloseRequest = ::exitApplication,
         title = "LLMS",
+        icon = icon
     ) {
         App()
     }
